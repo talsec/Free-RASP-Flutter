f153a8a17297de89604b0c6ad25a614c39ceabd0
Jaroslav Novotný <novotnyj912@gmail.com>
2023-01-27 13:59:57 +0100

    test: Updated tests

--- test/talsec_app_test.dart
+++ test/talsec_app_test.dart
@@ -2,11 +2,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:freerasp/talsec_app.dart';
 
 void main() {
-  final TalsecConfig config = TalsecConfig(
+  final config = TalsecConfig(
     androidConfig: AndroidConfig(
       expectedPackageName: 'PACKAGE_NAME',
-      expectedSigningCertificateHash:
-          'AKoRuyLMM91E7lX/Zqp3u4jMmd0A7hH/Iqozu0TMVd0=',
+      expectedSigningCertificateHashes: [
+        'AKoRuyLMM91E7lX/Zqp3u4jMmd0A7hH/Iqozu0TMVd0='
+      ],
     ),
     iosConfig: const IOSconfig(
       appBundleId: 'BUNDLE_ID',
@@ -14,23 +15,33 @@ void main() {
     ),
     watcherMail: 'john@example.com',
   );
-  const TalsecCallback callback = TalsecCallback();
+  const callback = TalsecCallback();
 
   test('Passing null values', () {
-    expect(() => TalsecApp(config: null, callback: callback),
-        throwsAssertionError);
     expect(
-        () => TalsecApp(config: config, callback: null), throwsAssertionError);
+      () => TalsecApp(config: null, callback: callback),
+      throwsAssertionError,
+    );
+    expect(
+      () => TalsecApp(config: config, callback: null),
+      throwsAssertionError,
+    );
   });
 
   test('Passing empty objects', () {
     expect(
-        () => TalsecApp(
-            config: TalsecConfig(watcherMail: null), callback: callback),
-        throwsAssertionError);
+      () => TalsecApp(
+        config: TalsecConfig(watcherMail: null),
+        callback: callback,
+      ),
+      throwsAssertionError,
+    );
 
-    // Callback CAN be unimplemented but at least [TalsecCallback] has to be provided.
-    expect(() => TalsecApp(config: config, callback: const TalsecCallback()),
-        returnsNormally);
+    // Callback CAN be unimplemented but at least [TalsecCallback] has to be
+    // provided.
+    expect(
+      () => TalsecApp(config: config, callback: const TalsecCallback()),
+      returnsNormally,
+    );
   });
 }
