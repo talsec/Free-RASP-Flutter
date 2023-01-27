f183627048a703866dea0a514a9a77d47378301d
Jaroslav Novotný <novotnyj912@gmail.com>
2023-01-27 14:00:58 +0100

    deps: New version of Talsec for iOS

--- ios/Debug/TalsecRuntime.xcframework/ios-arm64/TalsecRuntime.framework/Headers/CurlWrapper.h
+++ ios/Debug/TalsecRuntime.xcframework/ios-arm64/TalsecRuntime.framework/Headers/CurlWrapper.h
@@ -16,7 +16,7 @@
 #include <unistd.h>
 #include <string.h>
 
-struct ysLRNARmRJRU {
+struct lCRkBkeOPbmb {
     char *memory;
     size_t size;
     CURLcode ret;
--- ios/Release/TalsecRuntime.xcframework/ios-arm64_x86_64-simulator/TalsecRuntime.framework/Headers/CurlWrapper.h
+++ ios/Release/TalsecRuntime.xcframework/ios-arm64_x86_64-simulator/TalsecRuntime.framework/Headers/CurlWrapper.h
@@ -16,7 +16,7 @@
 #include <unistd.h>
 #include <string.h>
 
-struct RAUqKRKBVreA {
+struct gKSEtJMglUSg {
     char *memory;
     size_t size;
     CURLcode ret;
