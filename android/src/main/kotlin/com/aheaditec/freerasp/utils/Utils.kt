af634b2e24c65ed626bc18b6c4c12438a5c3419f
Jaroslav Novotný <novotnyj912@gmail.com>
2024-09-26 09:44:41 +0200

    feat: misc

--- android/src/main/kotlin/com/aheaditec/freerasp/utils/Utils.kt
+++ android/src/main/kotlin/com/aheaditec/freerasp/utils/Utils.kt
@@ -1,5 +1,6 @@
 package com.aheaditec.freerasp.utils
 
+import com.aheaditec.talsec_security.security.api.SuspiciousAppInfo
 import com.aheaditec.talsec_security.security.api.TalsecConfig
 import org.json.JSONException
 import org.json.JSONObject
@@ -11,33 +12,71 @@ internal class Utils {
                 throw JSONException("Configuration is null")
             }
             val json = JSONObject(configJson)
-            val androidConfig = json.getJSONObject("androidConfig")
-            val packageName = androidConfig.getString("packageName")
-            val certificateHashes = mutableListOf<String>()
-            val hashes = androidConfig.getJSONArray("signingCertHashes")
-            for (i in 0 until hashes.length()) {
-                certificateHashes.add(hashes.getString(i))
-            }
+
             val watcherMail = json.getString("watcherMail")
-            val alternativeStores = mutableListOf<String>()
-            if (androidConfig.has("supportedStores")) {
-                val stores = androidConfig.getJSONArray("supportedStores")
-                for (i in 0 until stores.length()) {
-                    alternativeStores.add(stores.getString(i))
-                }
-            }
             var isProd = true
             if (json.has("isProd")) {
                 isProd = json.getBoolean("isProd")
             }
+            val androidConfig = json.getJSONObject("androidConfig")
 
-            return TalsecConfig(
-                packageName,
-                certificateHashes.toTypedArray(),
-                watcherMail,
-                alternativeStores.toTypedArray(),
-                isProd
-            )
+            val packageName = androidConfig.getString("packageName")
+            val certificateHashes = androidConfig.extractArray<String>("signingCertHashes")
+            val alternativeStores = androidConfig.extractArray<String>("supportedStores")
+            val blocklistedPackageNames =
+                androidConfig.extractArray<String>("blocklistedPackageNames")
+            val blocklistedHashes = androidConfig.extractArray<String>("blocklistedHashes")
+            val whitelistedInstallationSources =
+                androidConfig.extractArray<String>("whitelistedInstallationSources")
+
+            val blocklistedPermissions = mutableListOf<Array<String>>()
+            if (androidConfig.has("blocklistedPermissions")) {
+                val permissions = androidConfig.getJSONArray("blocklistedPermissions")
+                for (i in 0 until permissions.length()) {
+                    val permission = permissions.getJSONArray(i)
+                    val permissionList = mutableListOf<String>()
+                    for (j in 0 until permission.length()) {
+                        permissionList.add(permission.getString(j))
+                    }
+                    blocklistedPermissions.add(permissionList.toTypedArray())
+                }
+            }
+
+            return TalsecConfig.Builder(packageName, certificateHashes)
+                .watcherMail(watcherMail)
+                .supportedAlternativeStores(alternativeStores)
+                .prod(isProd)
+                .blocklistedPackageNames(blocklistedPackageNames)
+                .blocklistedHashes(blocklistedHashes)
+                .blocklistedPermissions(blocklistedPermissions.toTypedArray())
+                .whitelistedInstallationSources(whitelistedInstallationSources)
+                .build()
+        }
+
+        fun fromTalsec(malwareInfo: List<SuspiciousAppInfo>) {
+            val packageInfoList = mutableListOf<String>()
+            for (info in malwareInfo) {
+                packageInfoList.add(info.toJson())
+            }
         }
     }
+}
+
+inline fun <reified T> JSONObject.extractArray(key: String): Array<T> {
+    val list = mutableListOf<T>()
+    if (this.has(key)) {
+        val jsonArray = this.getJSONArray(key)
+        for (i in 0 until jsonArray.length()) {
+            val element = when (T::class) {
+                String::class -> jsonArray.getString(i) as T
+                Int::class -> jsonArray.getInt(i) as T
+                Double::class -> jsonArray.getDouble(i) as T
+                Boolean::class -> jsonArray.getBoolean(i) as T
+                Long::class -> jsonArray.getLong(i) as T
+                else -> throw IllegalArgumentException("Unsupported type")
+            }
+            list.add(element)
+        }
+    }
+    return list.toTypedArray()
 }
