9bfa6dfa1c6219f2644e52274bd3cd54c746b282
Jaroslav Novotný <novotnyj912@gmail.com>
2024-10-01 15:59:35 +0200

    docs: adjust exports

--- lib/freerasp.dart
+++ lib/freerasp.dart
@@ -1,5 +1,6 @@
 export 'src/enums/enums.dart';
 export 'src/errors/errors.dart';
+export 'src/generated/talsec_pigeon_api.g.dart' show SuspiciousAppInfo, PackageInfo;
 export 'src/models/models.dart';
 export 'src/talsec.dart';
 export 'src/threat_callback.dart';
