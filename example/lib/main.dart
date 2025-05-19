50f8c2cb5462c62a4ca9f160d69371eb4f543132
Tomas Psota <to.psota@gmail.com>
2025-05-19 12:46:54 +0200

    chore: remove unused import

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: public_member_api_docs, avoid_redundant_argument_values
 
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freerasp/freerasp.dart';
