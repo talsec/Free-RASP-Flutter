0f7c92e4fea3ca0c1b5747f962fd19c87eb35550
Jaroslav Novotný <62177414+yardexx@users.noreply.github.com>
2025-07-10 15:54:37 +0200

    fix

--- ios/freerasp/Package.swift
+++ ios/freerasp/Package.swift
@@ -6,21 +6,21 @@ import PackageDescription
 let package = Package(
     name: "freerasp",
     platforms: [
-        .iOS("12.0"),
+        .iOS("12.0")
     ],
     products: [
         .library(name: "freerasp", targets: ["freerasp"])
     ],
     dependencies: [],
     targets: [
+        .binaryTarget(
+            name: "TalsecRuntime",
+            path: "./TalsecRuntime.xcframework"
+        ),
         .target(
             name: "freerasp",
-            dependencies: [],
+            dependencies: ["TalsecRuntime"],
             resources: []
         ),
-        .binaryTarget(
-            name: "TalsecRuntime.xcframework",
-            path: "./TalsecRuntime.xcframework"
-        )
     ]
 )
