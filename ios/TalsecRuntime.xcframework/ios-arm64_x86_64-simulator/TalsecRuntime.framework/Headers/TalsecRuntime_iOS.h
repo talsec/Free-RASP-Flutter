19770160771731fb5043b4c0db3225293cbcf240
Jaroslav Novotný <62177414+yardexx@users.noreply.github.com>
2025-07-18 13:35:54 +0200

    fix: runtime framework

---
+++ ios/TalsecRuntime.xcframework/ios-arm64_x86_64-simulator/TalsecRuntime.framework/Headers/TalsecRuntime_iOS.h
@@ -0,0 +1,23 @@
+//
+//  TalsecRuntime.h
+//  Talsec
+//
+//  Created by Jakub Mejtský on 15/12/2018.
+//  Copyright © 2018 AHEAD iTec, s.r.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TalsecRuntime_iOS.
+FOUNDATION_EXPORT double TalsecRuntime_iOSVersionNumber;
+
+//! Project version string for TalsecRuntime.
+FOUNDATION_EXPORT const unsigned char TalsecRuntime_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Talsec/PublicHeader.h>
+
+#define __warning__(x)
+
+#import "stdcheaders.h"
+#import "mprintf.h"
+#import "curl.h"
