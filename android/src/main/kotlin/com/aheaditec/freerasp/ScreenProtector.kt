4e850fadd7510a8476449cbac1d05372f766c781
Jaroslav Novotný <62177414+yardexx@users.noreply.github.com>
2025-10-09 12:48:26 +0200

    fix: crashing sdk caused by screen protector

--- android/src/main/kotlin/com/aheaditec/freerasp/ScreenProtector.kt
+++ android/src/main/kotlin/com/aheaditec/freerasp/ScreenProtector.kt
@@ -15,42 +15,39 @@ import com.aheaditec.talsec_security.security.api.Talsec
 import io.flutter.Log
 import java.util.function.Consumer
 
-internal class ScreenProtector : DefaultLifecycleObserver {
-    companion object {
-        private const val TAG = "ScreenProtector"
-        private const val SCREEN_CAPTURE_PERMISSION = "android.permission.DETECT_SCREEN_CAPTURE"
-        private const val SCREEN_RECORDING_PERMISSION = "android.permission.DETECT_SCREEN_RECORDING"
-    }
+@SuppressLint("StaticFieldLeak")
+internal object ScreenProtector {
+    private const val TAG = "TalsecScreenProtector"
+    private const val SCREEN_CAPTURE_PERMISSION = "android.permission.DETECT_SCREEN_CAPTURE"
+    private const val SCREEN_RECORDING_PERMISSION = "android.permission.DETECT_SCREEN_RECORDING"
 
-    internal var activity: Activity? = null
     private var isEnabled = false
+    private var isRegistered = false
 
-    private val screenCaptureCallback = ScreenCaptureCallback { Talsec.onScreenshotDetected() }
+    private val cachedThreats = mutableSetOf<Threat>()
+    private val screenCaptureCallback = ScreenCaptureCallback { handleThreat(Threat.Screenshot) }
     private val screenRecordCallback: Consumer<Int> = Consumer<Int> { state ->
-        if (state == SCREEN_RECORDING_STATE_VISIBLE) {
-            Talsec.onScreenRecordingDetected()
-            Log.e("ScreenProtector", "Screen recording detected")
-        }
+        if (state == SCREEN_RECORDING_STATE_VISIBLE) handleThreat(Threat.ScreenRecording)
     }
 
-    internal fun enable() {
+    fun enable() {
         if (isEnabled) return
+
         isEnabled = true
+        cachedThreats.forEach { handleThreat(it) }
+        cachedThreats.clear()
     }
 
-    override fun onStart(owner: LifecycleOwner) {
-        super.onStart(owner)
-
-        if (isEnabled) activity?.let { register(it) }
+    fun disable() {
+        isEnabled = false
     }
 
-    override fun onStop(owner: LifecycleOwner) {
-        super.onStop(owner)
+    internal fun register(activity: Activity) {
+        if (isRegistered) {
+            android.util.Log.w(TAG, "ScreenProtector is already registered.")
+            return
+        }
 
-        if (isEnabled) activity?.let { unregister(it) }
-    }
-
-    private fun register(activity: Activity) {
         if (Build.VERSION.SDK_INT >= 34) {
             registerScreenCapture(activity)
         }
@@ -58,27 +55,8 @@ internal class ScreenProtector : DefaultLifecycleObserver {
         if (Build.VERSION.SDK_INT >= 35) {
             registerScreenRecording(activity)
         }
-    }
 
-    // Missing permission is suppressed because the decision to use the screen capture API is made
-    // by developer, and not enforced by the library.
-    @SuppressLint("MissingPermission")
-    private fun unregister(currentActivity: Activity) {
-        val context = currentActivity.applicationContext
-
-        if (Build.VERSION.SDK_INT >= 34 && hasPermission(
-                context, SCREEN_CAPTURE_PERMISSION
-            )
-        ) {
-            currentActivity.unregisterScreenCaptureCallback(screenCaptureCallback)
-        }
-
-        if (Build.VERSION.SDK_INT >= 35 && hasPermission(
-                context, SCREEN_RECORDING_PERMISSION
-            )
-        ) {
-            currentActivity.windowManager?.removeScreenRecordingCallback(screenRecordCallback)
-        }
+        isRegistered = true
     }
 
     // Missing permission is suppressed because the decision to use the screen capture API is made
@@ -112,7 +90,34 @@ internal class ScreenProtector : DefaultLifecycleObserver {
             context.mainExecutor, screenRecordCallback
         )
         screenRecordCallback.accept(initialState)
+    }
 
+    // Missing permission is suppressed because the decision to use the screen capture API is made
+    // by developer, and not enforced by the library.
+    @SuppressLint("MissingPermission")
+    internal fun unregister(currentActivity: Activity) {
+        if (!isRegistered) {
+            android.util.Log.w(TAG, "ScreenProtector is not registered.")
+            return
+        }
+
+        val context = currentActivity.applicationContext
+
+        if (Build.VERSION.SDK_INT >= 34 && hasPermission(
+                context, SCREEN_CAPTURE_PERMISSION
+            )
+        ) {
+            currentActivity.unregisterScreenCaptureCallback(screenCaptureCallback)
+        }
+
+        if (Build.VERSION.SDK_INT >= 35 && hasPermission(
+                context, SCREEN_RECORDING_PERMISSION
+            )
+        ) {
+            currentActivity.windowManager?.removeScreenRecordingCallback(screenRecordCallback)
+        }
+
+        isRegistered = false
     }
 
     private fun hasPermission(context: Context, permission: String): Boolean {
@@ -122,9 +127,22 @@ internal class ScreenProtector : DefaultLifecycleObserver {
     }
 
     private fun reportMissingPermission(protectionType: String, permission: String) {
-        Log.e(
+        android.util.Log.e(
             TAG,
             "Failed to register $protectionType callback. Check if $permission permission is granted in AndroidManifest.xml"
         )
+    }
+
+    private fun handleThreat(threat: Threat) {
+        if (!isEnabled) {
+            cachedThreats.add(threat)
+            return
+        }
+
+        when (threat) {
+            Threat.Screenshot -> Talsec.onScreenshotDetected()
+            Threat.ScreenRecording -> Talsec.onScreenRecordingDetected()
+            else -> throw IllegalArgumentException("Unexpected Threat type: $threat")
+        }
     }
 }
